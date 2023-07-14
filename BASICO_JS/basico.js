5f8caadcc8563359024b024c380ac8f1e704ed40
Kamy <kamyllamoraisviana@hotmail.com>
2023-07-13 21:52:40 -0300

    .

---
+++ BASICO_JS/basico.js
@@ -0,0 +1,28 @@
+
+//w3schools
+//mdn web docs
+
+console.log ("Hello World no arquivo js");
+console.log (1 + "1");
+console.log (2- "1");
+
+
+//variaveis 
+
+var x = 10 ;
+let y = 11 ; 
+
+// a var pode ser utilizada várias vezes enquanto o y da erro. 
+console.log ((x+x)/y);
+
+//Para saber o tipo de variável 
+console.log (typeof (x));
+console.log ("aula" *2);
+let numero = Number (50);
+console.log (typeof (numero));
+let numeroString = String (50);
+let casaDecimal = 10.998;
+
+console.log (typeof(numero));
+console.log (typeof(numeroString));
+console.log (typeof(casaDecimal));
